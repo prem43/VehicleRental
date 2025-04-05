e05680f9c6d96122badeef20f86bdc7227798be7
Prem ranjan <8294138828.pr@gmail.com>
2025-04-05 18:58:34 +0530

    Add project files.

---
+++ VehicleRental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
